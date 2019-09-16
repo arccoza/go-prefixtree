0de0ee467fdbcc1309d188ec0a1e609c014c79c1
Adrien <adrien@arccoza.com>
2019-09-16 10:35:40 +0900

    Move Node type into its own file

--- main.go
+++ main.go
@@ -21,44 +21,6 @@ func main() {
   fmt.Println(pm.Get("a112").Values())
 }
 
-type Node struct {
-  Children map[string]*Node
-  Value    interface{}
-}
-
-func NewNode() *Node {
-  return &Node{make(map[string]*Node), nil}
-}
-
-func (n *Node) Values() []interface{} {
-  all := make([]interface{}, 0, len(n.Children) + 1)
-  all = append(all, n.Value)
-  nodes := make([]Node, 0, 10)
-  nodes = append(nodes, *n)
-  
-  for len(nodes) > 0 {
-    n := &nodes[0]
-    nodes = nodes[1:]
-
-    for _, v := range n.Children {
-      if len(v.Children) > 0 {
-        nodes = append(nodes, *v)
-      }
-      all = append(all, v.Value)
-    }
-  }
-
-  return all
-}
-
-// func (n *Node) String() string {
-//   s := fmt.Sprintf("%v", n.Value)
-//   for k, v := range n.Children {
-//     s += fmt.Sprintf("\n>>%v: %v", k, v)
-//   }
-//   return s
-// }
-
 type PrefixTree Node
 
 func (pm *PrefixTree) Get(key string) *Node {
