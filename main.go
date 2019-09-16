2bc947412c91a1580f301a374fbc325a993e8be3
Adrien <adrien@arccoza.com>
2019-09-16 10:36:56 +0900

    Move PrefixTree type into its own file

--- main.go
+++ main.go
@@ -20,38 +20,3 @@ func main() {
   // fmt.Println(pm)
   fmt.Println(pm.Get("a112").Values())
 }
-
-type PrefixTree Node
-
-func (pm *PrefixTree) Get(key string) *Node {
-  path := []rune(key)
-  n := (*Node)(pm)
-
-  for i := range path {
-    v := string(path[i])
-    m, ok := n.Children[v]
-    if !ok {
-      return nil
-    }
-    n = m
-  }
-  
-  return n
-}
-
-func (pm *PrefixTree) Set(key string, value interface{}) {
-  path := []rune(key)
-  n := (*Node)(pm)
-
-  for i := range path {
-    v := string(path[i])
-    m, ok := n.Children[v]
-    if !ok {
-      m = NewNode()
-    }
-    n.Children[v] = m
-    n = m
-  }
-  
-  n.Value = value
-}
